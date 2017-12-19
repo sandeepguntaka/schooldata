2b611e2b9781bac2d4f363dcdcefb1a86d73b7e3
Sandeep Reddy <sandeepkmr70@gmail.com>
2017-12-19 18:23:39 +0530

    sa

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :schooldata, Schooldata.Endpoint,
   load_from_system_env: true,
-  http: [port: 80]
-#  cache_static_manifest: "priv/static/cache_manifest.json",
-#  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  url: [scheme: "https", host: "cryptic-sea-40056.herokuapp.com", port: System.get_env("PORT")],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 
 # Do not print debug messages in production
