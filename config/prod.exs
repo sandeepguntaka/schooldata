660bbd0030e5607538f415259c368ab683f829e1
Sandeep Reddy <sandeepkmr70@gmail.com>
2017-12-19 18:07:20 +0530

    sa

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :schooldata, Schooldata.Endpoint,
   load_from_system_env: true,
-  http: [ip: {0,0,0,0}, port: 8080]
+  http: [ip: {0,0,0,0}, port: 80]
 #  cache_static_manifest: "priv/static/cache_manifest.json",
 #  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
