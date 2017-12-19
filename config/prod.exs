bba80262704b66c4ba88c79e41f31ab48f365491
Sandeep Reddy <sandeepkmr70@gmail.com>
2017-12-19 12:58:29 +0530

    sa

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :schooldata,
        url: [
          scheme: "https",
          host: "cryptic-sea-40056.herokuapp.com",
-         port: System.getenv("PORT")
+         port: System.get_env("PORT"),
        ],
        force_ssl: [
          rewrite_on: [:x_forwarded_proto]
@@ -71,8 +71,8 @@ config :logger, level: :info
 # which should be versioned separately.
 #import_config "prod.secret.exs"
 config :schooldata,
-       Schooldata.Repo,
-       adapter: Ecto.Adapters.Postgres,
-       url: System.get_env("DATABASE_URL"),
-       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-       ssl: true
+  Schooldata.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
