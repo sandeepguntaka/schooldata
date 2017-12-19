6ea990f1adfde91e207ba1d744576c7e57d7d1c4
Sandeep Reddy <sandeepkmr70@gmail.com>
2017-12-19 13:02:52 +0530

    sa

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :schooldata,
        url: [
          scheme: "https",
          host: "cryptic-sea-40056.herokuapp.com",
-         port: System.get_env("PORT"),
+         port: {:system, "PORT"},
        ],
        force_ssl: [
          rewrite_on: [:x_forwarded_proto]
