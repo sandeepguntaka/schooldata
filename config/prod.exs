cda155df3efba816157439bfcc0a69c24664085c
Sandeep Reddy <sandeepkmr70@gmail.com>
2017-12-19 12:42:02 +0530

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :schooldata, Schooldata.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "still-hamlet-76101.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "cryptic-sea-40056.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
