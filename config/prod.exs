6c37a9a8f1771dd049a13375fd3014f359cbf8f9
Sandeep Reddy <sandeepkmr70@gmail.com>
2017-12-20 11:01:01 +0530

    sa

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ config :schooldata, Schooldata.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
-  
+
 # Do not print debug messages in production
 config :logger, level: :info
 
@@ -59,6 +59,7 @@ config :logger, level: :info
 #
 #     config :schooldata, Schooldata.Endpoint, server: true
 #
+
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
